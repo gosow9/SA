c2563200200d4abf767c9b6711a7ca786909c789
Tischhauser Manuel <mtischha@hsr.ch>
2019-10-22 14:17:23 +0200

    solarpanel

---
+++ Technologie/EnergyHarvesting/Solarpanel/PCE_measurement.py
@@ -0,0 +1,28 @@
+import serial
+import time
+import struct
+
+baudrate = 9600 #whatever baudrate you are listening to
+com_port2 = 'COM13' #replace with your com port path
+
+
+sniffer_PCE = serial.Serial(com_port2, baudrate, timeout = 1)
+time.sleep(1)
+sniffer_PCE.reset_input_buffer()
+f = open('measuredData.txt','w')
+while 1:
+  try:
+    sniffer_PCE.write(serial.to_bytes('\x11'.encode('utf-8')))
+    data_PCE = sniffer_PCE.read(19); 
+    #~ print(','.join([('%02X'%x) for x in data_PCE])+'\n')
+    #~ print(data_PCE[10:12])
+    data_PCE_int = data_PCE[10]*100 + data_PCE[11]
+    print(data_PCE_int)
+    f.write('%d\n'%data_PCE_int)
+    #~ lux = ''.join([('%02x'%X) for X in data_PCE[10:12]])
+    #~ print(lux)
+    time.sleep(1)
+  except (KeyboardInterrupt, SystemExit):
+      print('interrupted')
+      exit()
+f.close()
