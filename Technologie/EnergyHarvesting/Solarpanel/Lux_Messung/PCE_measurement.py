36b0c5da2e5170b5bf3b60af4558e68a896f2614
manitisch <49937592+manitisch@users.noreply.github.com>
2019-10-24 16:48:00 +0200

    lux-messung

--- Technologie/EnergyHarvesting/Solarpanel/Lux_Messung/PCE_measurement.py
+++ Technologie/EnergyHarvesting/Solarpanel/Lux_Messung/PCE_measurement.py
@@ -1,6 +1,8 @@
 import serial
 import time
 import struct
+import datetime
+
 
 baudrate = 9600 #whatever baudrate you are listening to
 com_port2 = 'COM5' #replace with your com port path
@@ -10,6 +12,7 @@ sniffer_PCE = serial.Serial(com_port2, baudrate, timeout=1)
 time.sleep(1)
 sniffer_PCE.reset_input_buffer()
 f = open('measuredData.txt', 'w')
+
 while 1:
   try:
     sniffer_PCE.write(serial.to_bytes('\x11'.encode('utf-8')))
@@ -18,11 +21,12 @@ while 1:
     #~ print(data_PCE[10:12])
     data_PCE_int = data_PCE[10]*100 + data_PCE[11]
     print(data_PCE_int)
-    f.write('%d\n'%data_PCE_int)
+    #f.write('%d\n'%data_PCE_int)
+    f.write('{:d}, {}\n'.format(data_PCE_int, str(datetime.datetime.now())))
     #~ lux = ''.join([('%02x'%X) for X in data_PCE[10:12]])
     #~ print(lux)
     time.sleep(1)
   except (KeyboardInterrupt, SystemExit):
       print('interrupted')
       exit()
-f.close()
+      f.close()
