4bab0a1240fb7aa7e281c347574fd78e5ccf4e85
manitisch <49937592+manitisch@users.noreply.github.com>
2019-11-12 16:19:26 +0100

    messungen_plot

---
+++ Doku/Dokumentation/plots/messungen_plot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 16:14:06 2019
+
+@author: Manuel
+"""
+
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+plt.style.use("ggplot")
+
+with f as open('test.txt')
