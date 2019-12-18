3649c5ca9568d7081a15054a6060a5be830736e1
manitisch <49937592+manitisch@users.noreply.github.com>
2019-12-18 18:06:56 +0100

    plots

--- Doku/Bericht/5-results/functionality/plot/plot.py
+++ Doku/Bericht/5-results/functionality/plot/plot.py
@@ -11,7 +11,7 @@ def avfilter(x, M): #M immer ungerade
 data = pd.read_csv('scope_27.csv')
 
 
-t = pd.DataFrame.to_numpy(data.second).reshape(-1,1)[:,0]
+t_r = pd.DataFrame.to_numpy(data.second).reshape(-1,1)[:,0]
 v_bat = pd.DataFrame.to_numpy(data.Volt1).reshape(-1,1)[:,0]
 v_step = pd.DataFrame.to_numpy(data.Volt2).reshape(-1,1)[:,0]
 v_ldo = pd.DataFrame.to_numpy(data.Volt3).reshape(-1,1)[:,0]
@@ -19,25 +19,27 @@ v_disp = pd.DataFrame.to_numpy(data.Volt4).reshape(-1,1)[:,0]
 
 
 
-
 #Mitteln (ÜBERGABEPARAMETER = 1 entspricht keine mittelung)
-v_bat = avfilter(v_bat, 1)
-v_step = avfilter(v_step, 1)
-v_ldo = avfilter(v_ldo, 1)
-v_disp = avfilter(v_disp, 1)
+v_bat = avfilter(v_bat, 3)
+v_step = avfilter(v_step, 3)
+v_ldo = avfilter(v_ldo, 3)
+v_disp = avfilter(v_disp, 3)
 
 
 #zuschneiden (l,r für links und recht abschneiden)
-l = 0
-r = 20
-org_len = np.size(t)
+l = 2500
+r = 1500
+org_len = np.size(t_r)
 
-t = t[l:org_len-r]
+t_r = t_r[l:org_len-r]
 v_bat = v_bat[l:org_len-r]
 v_step = v_step[l:org_len-r]
 v_ldo = v_ldo[l:org_len-r]
 v_disp = v_disp[l:org_len-r]
 
+#zeitaches erstellen
+T = t_r[1]-t_r[0]
+t = np.arange(0, np.size(t_r))*T
 
 # use LaTeX fonts in the plot
 plt.rcParams['font.family'] = 'STIXGeneral'
@@ -46,11 +48,16 @@ plt.rcParams.update({'font.size': 12})
 #plot
 plt.plot(t, v_bat, label='$V_{\mathbf{BAT}}$')
 plt.plot(t, v_step, label='$V_{\mathbf{BOOT}}$')
-plt.plot(t, v_ldo, label='$V_{\mathbf{LDO}}$')
-plt.plot(t, v_disp, label='$V_{\mathbf{DISP}}$')
+plt.plot(t, v_ldo, label='$V_{\mathbf{LDO}}$', color='tab:red')
+#plt.plot(t, v_disp, label='$V_{\mathbf{DISP}}$', color='tab:red')
+plt.xlim([0, 12])
+plt.ylim([0, 4])
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
+
 plt.grid(True)
 
 plt.tight_layout()
-plt.legend(bbox_to_anchor=(0.7, 0.5))
+plt.legend(bbox_to_anchor=(0.75, 0.55))
 
-#plt.savefig('vboot.pdf')
+plt.savefig('v.pdf')
